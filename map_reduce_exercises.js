abccc3bee95120b8380b4e2f24c7e4675f6e2c3d
Roberra0 <roberra.aklilu@gmail.com>
2023-07-05 21:15:58 -0700

    Completed array cardio day 1

---
+++ map_reduce_exercises.js
@@ -0,0 +1,24 @@
+const people = ['Smith, John',
+'Jones, Jane',
+'Brown, Mary',
+'Green, Michael',
+'White, Susan',
+'Black, David',
+'Blue, Emily',
+'Gray, Peter',
+'Pink, Sarah',
+'Purple, William'];
+
+const inventors = [
+ {first: 'John', last: 'Doe', year: 1991, passed: 2005},
+ {first: 'Jane', last: 'Smith', year: 1992, passed: 2006},
+ {first: 'Michael', last: 'Brown', year: 1993, passed: 2007},
+ {first: 'Susan', last: 'Green', year: 1994, passed: 2008},
+ {first: 'David', last: 'Black', year: 1995, passed: 2009},
+ {first: 'Emily', last: 'Blue', year: 1996, passed: 2010},
+ {first: 'Peter', last: 'Gray', year: 1997, passed: 2011},
+ {first: 'Sarah', last: 'Pink', year: 1998, passed: 2012},
+ {first: 'William', last: 'Purple', year: 1999, passed: 2013},
+ {first: 'Amelia', last: 'Rose', year: 2000, passed: 2014}];
+
+//Filter inventors who were born in 1500s
